b3a71ced9cde7c1711b66b4852128e7802b18611
Leo Baker-Hytch <l.bakerhytch@gmail.com>
2014-10-18 17:01:59 +0100

    Update controller to make Bitcoin investment calculations

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,32 @@
 var bitcoinCalculator = angular.module('bitcoinCalculator', []);
 
 // Bind the controller with the view
-bitcoinCalculator.controller('bitcoinController', ['$scope', function($scope) {
-    // Attach the number ‘0’ to the DOM
-    $scope.somenumber = 0;
+bitcoinCalculator.controller('bitcoinController', ['$scope', '$http', function($scope, $http) {
+
+    $scope.currentRate = 1000;
+    $scope.initialAmount = 2500;
+
+    $scope.bitcoinsHeld = function() {
+        return $scope.initialAmount / $scope.currentRate;
+    };
+
+    $scope.newAmount = function(price) {
+        return price / $scope.currentRate * $scope.initialAmount;
+    };
+
+    $scope.profit = function(price) {
+        return price / $scope.currentRate * $scope.initialAmount - $scope.initialAmount;
+    };
+
+    $http.get('https://bitpay.com/api/rates')
+        .success(function(data) {
+
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].code === 'GBP') {
+                    $scope.currentRate = data[i]['rate'];
+                    break;
+                }
+            }
+        });
+
 }]);
